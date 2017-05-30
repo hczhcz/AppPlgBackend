4c3363e77fd9d4072e5b4eca4ced0b4a5cbfbe9b
Laurence Liu <liuxy6@gmail.com>
2017-05-31 00:35:56 +0800

    Fix format

--- action.go
+++ action.go
@@ -1,24 +1,24 @@
 package main
 
 func actionSessionInit() (string, interface{}) {
-    type response struct {
-        SessionID string `json:"session_id"`
-    }
+	type response struct {
+		SessionID string `json:"session_id"`
+	}
 
-    return "session_init", response{"0000"} // TODO: mock
+	return "session_init", response{"0000"} // TODO: mock
 }
 
 func actionInvalidRequest(hint string) (string, interface{}) {
-    type response struct {
-        Hint string `json:"hint"`
-    }
+	type response struct {
+		Hint string `json:"hint"`
+	}
 
-    return "invalid_request", response{hint}
+	return "invalid_request", response{hint}
 }
 
 func actionInternalError() (string, interface{}) {
-    type response struct {
-    }
+	type response struct {
+	}
 
-    return "internal_error", response{}
+	return "internal_error", response{}
 }
