b31ff83c6cb08fe023a113c476bbb88fd7c4dcef
Laurence Liu <liuxy6@gmail.com>
2017-05-31 20:29:17 +0800

    Fix impl error interface for ErrorDuplicatedEmail

--- error.go
+++ error.go
@@ -3,6 +3,6 @@ package main
 type ErrorDuplicatedEmail struct { // TODO: ONLY for testing
 }
 
-func (e *ErrorDuplicatedEmail) Error() string {
+func (e ErrorDuplicatedEmail) Error() string {
 	return "Duplicated email"
 }
