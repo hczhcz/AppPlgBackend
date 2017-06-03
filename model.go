afb89aac40fd78cab18112e9f28cc011c185c9d7
Laurence Liu <liuxy6@gmail.com>
2017-06-03 21:28:33 +0800

    DB

--- model.go
+++ model.go
@@ -5,9 +5,12 @@ import (
 	"log"
 )
 
+import "github.com/go-pg/pg"
+
 type Login struct {
-	Email string `json:"email,omitempty"`
-	Phone string `json:"phone,omitempty"`
+	ID    uint64 `json:"-"`
+	Email string `sql:",null" json:"email,omitempty"`
+	Phone string `sql:",null" json:"phone,omitempty"`
 }
 
 func (l Login) IsValid() error {
@@ -23,13 +26,46 @@ func (l Login) IsValid() error {
 }
 
 type User struct {
+	ID          uint64 `json:"-"`
 	Nickname    string `json:"nickname,omitempty"`
 	Gender      string `json:"gender,omitempty"`
 	Description string `json:"description,omitempty"`
 }
 
+var DBConn *pg.DB
+
+func init() {
+	DBConn = pg.Connect(&pg.Options{
+		Addr:     "127.0.0.1:5432",
+		User:     "hyclz",
+		Password: "666666", // FIXME
+		Database: "appplg",
+	})
+}
+
 func UserNew(login Login, user User) error {
 	log.Println("Create user:", login, user)
+
+	tx, err := DBConn.Begin()
+	if err != nil {
+		return err
+	}
+
+	if err := tx.Insert(&login); err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	user.ID = login.ID
+	if err := tx.Insert(&user); err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	if err := tx.Commit(); err != nil {
+		tx.Rollback()
+		return err
+	}
 
 	if login.Email != "" {
 		log.Println("Sent mail to:", login.Email)
@@ -65,5 +101,5 @@ func UserVerify(sessionID string, code string) (uint64, error) {
 func UserGet(userID uint64) (User, error) {
 	log.Println("Get user_id", userID)
 
-	return User{"test", "male", "Not human"}, nil
+	return User{0, "test", "male", "Not human"}, nil
 }
